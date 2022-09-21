e83dec0bc215a3e4cc1f183af2f2598194b624d4
rpuckooo <90442421+rpuckooo@users.noreply.github.com>
2022-09-21 09:07:13 +0300

    my first commit

---
+++ game.py
@@ -0,0 +1,13 @@
+human_turn = "rock"
+computer_turn = 'scissors'
+
+if human_turn == computer_turn:
+    print ('draw!')
+elif human_turn == 'rock' and computer_turn == 'scissors' : # human wins 
+    print ('human wins!')
+elif human_turn == 'paper' and computer_turn == 'rock' :
+    print ('human wins!')
+elif human_turn == 'scissors' and computer_turn == 'paper' :
+    print ('human wins!')
+else:
+    print ('computer wins!')
