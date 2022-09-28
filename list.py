cff3feff17a4cc1e37943589defdf839c5134630
rpuckooo <90442421+rpuckooo@users.noreply.github.com>
2022-09-28 09:36:18 +0300

    Add files via upload

---
+++ list.py
@@ -0,0 +1,9 @@
+names = ['Anna', 'Jenifer', 'Mik', 'Laura', 'Oskars' ]
+ages = [34,68,16,34,67]
+
+# print (len(ages))
+
+index = 4 
+
+print(names[index])
+print(ages[index])
--- rps_game.py
+++ rps_game.py
@@ -1,5 +1,7 @@
 import random
 turns = [ 'rock', 'paper', 'scissors']
+human_turns = []   # empty list
+computer_turns = [] # empty list
 
 while(True):
     human_turn = input("enter your turn, or type exit: ")
@@ -8,6 +10,10 @@ while(True):
     if human_turn == 'exit':
         print('thx for playing, bye bye !')
         break
+    
+    human_turns.append(human_turn)
+    computer_turns.append(computer_turn)
+
 
     print(f'Human:{human_turn} vs. Computer: {computer_turn}')
     if human_turn == computer_turn:
@@ -20,3 +26,7 @@ while(True):
         print ('human wins!')
     else:
         print ('computer wins!')
+
+print(f'you have played {len(human_turn)} times')
+print(human_turns)
+print(computer_turns)
